d2a301913d96d0513357e12d4b33f761bd8be35e
Tajinderens <tonysingh1203@gmail.com>
2025-05-08 19:16:55 -0400

    Created meida.ts data file simmilar to the projects.ts one

---
+++ src/data/media.ts
@@ -0,0 +1,49 @@
+import { Media } from '../components/MediaCard';
+
+export const projects: Media[] = [
+  {
+    id: '1',
+    title: 'AI2Web3Bootcamp NYC',
+    description: 'Our content across platforms',
+    imageUrl: '/project_assets/ai2web3bootcamp_banner.png',
+    url: 'https://github.com/Wagademy/AI2Web3',
+    type: 'twitter',
+    tags: ['Education', 'AI', 'Web3']
+  },
+  {
+    id: '2',
+    title: 'Evrica Technologies Inc.',
+    description: 'All-in-one ecosystem of tools and communities, bridging gaps between web2 to web3.',
+    imageUrl: '/project_assets/evrica_app_img.jpg',
+    url: 'https://github.com/EvricaTechnologiesInc/',
+    type: 'workshop',
+    tags: ['Asset Management', 'AI', 'Blockchain', 'Entrepreneurship', 'Web3', 'NFT']
+  },
+  {
+    "id": "3",
+    "title": "Ambient Commerce",
+    "description": "Agent-to-Agent Commerce, Onchain. Experience live negotiations between buyer and seller agents, leading to automated on-chain transactions.",
+    "imageUrl": "https://pbs.twimg.com/profile_images/1893128410257293312/W9c0ZRrc_400x400.jpg",
+    "url": "https://x.com/ambientcommerce",
+    "type": 'twitter',
+    "tags": ["E-commerce", "Blockchain", "Automation", "AI", "Web3"]
+  },
+  {
+    "id": "4",
+    "title": "Byte_AI",
+    "description": "The first fully autonomous Delivery Agent in the world",
+    "imageUrl": "https://pbs.twimg.com/profile_images/1869624181366915072/VfivRpYm_400x400.png",
+    "url": "https://x.com/Byte__AI",
+    "type": 'workshop',
+    "tags": ["Blockchain", "Automation", "AI", "Web3", "Delivery"]
+  },
+  {
+    "id": "5",
+    "title": "Lynkable",
+    "description": "A hybrid community platform that connects local event organizers, artists, and small businesses to strengthen community events.",
+    "imageUrl": "/project_assets/lynkable-logo.avif",
+    "url": "https://github.com/buildcity/lynkable-project",
+    "type": 'article',
+    "tags": ["Community", "Events", "Local Business", "Social Platform"]
+  },
+];
