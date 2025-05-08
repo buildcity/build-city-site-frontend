9a1287d97a62c1cde363cdf4613fb6456a9878e4
Tajinderens <tonysingh1203@gmail.com>
2025-05-08 18:44:27 -0400

    Added Youtube and instagram to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Github, Twitter, MessageCircle, Calendar } from 'lucide-react';
+import { Github, Twitter, MessageCircle, Calendar, Youtube, Instagram } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-8">
+    <footer className="bg-black text-white py-8 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
@@ -37,12 +37,28 @@ const Footer = () => {
               <MessageCircle className="h-6 w-6" />
             </a>
             <a
+              href="https://www.youtube.com/@buidlcity"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transform hover:scale-110 transition-transform duration-200"
+            >
+              <Youtube className="h-6 w-6" />
+            </a>
+            <a
               href="https://lu.ma/buildcity"
               target="_blank"
               rel="noopener noreferrer"
               className="transform hover:scale-110 transition-transform duration-200"
             >
               <Calendar className="h-6 w-6" />
+            </a>
+            <a
+              href="https://www.instagram.com/build__city?igsh=NHI1YnVrbDV5cGp6"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transform hover:scale-110 transition-transform duration-200"
+            >
+              <Instagram className="h-6 w-6" />
             </a>
           </div>
         </div>
