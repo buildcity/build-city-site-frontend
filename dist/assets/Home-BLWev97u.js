c4ed6651bcbf1ebff823dc3b8310977408312994
Tajinderens <tonysingh1203@gmail.com>
2025-05-13 21:11:26 -0400

    Replace header/footer textwith logo and adjust spacing

--- dist/assets/Home-BLWev97u.js
+++ dist/assets/Home-BLWev97u.js
@@ -1,4 +1,4 @@
-import{c as r,r as s,j as e}from"./index-dN33j0SM.js";/**
+import{c as r,r as s,j as e}from"./index-BzMAqnby.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,15 @@ const Footer = () => {
     <footer className="bg-black text-white py-8 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0">
-            <h3 className="text-xl font-bold">BUILDCITY</h3>
-            <p className="text-gray-400">Building the future of communities</p>
-          </div>
+        <div className="mb-4 md:mb-0 flex flex-col items-center md:items-start leading-none">
+  <img
+    src="/buildcitylogo_assets/buildcitylogo.png" 
+    alt="Build City Logo"
+    className="h-40 w-35 mt-.5"
+  />
+
+</div>
+
           
           <div className="flex space-x-6">
             <a
